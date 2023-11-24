e16f761b0c3ab36a727646863b1df997bb90e44d
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-24 10:22:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-const parseCode = (str) => {
+
   // your code here
-	const [firstName, lastName, id]=str.split("000");
-	
-	return { firstName, lastName, id };
+	const parseCode = (str) => {
+  // your code here
+	let f_Name="";
+	let l_name="";
+	let id="";
+	let i=0;
+	while(str.charAt(i)!=="0"){
+		f_Name+=str.charAt(i);
+		i++;
+	}
+	while(str.charAt(i)==="0"){
+		i++;
+	}
+	while(str.charAt(i)!=="0"){
+		l_name+=str.charAt(i);
+		i++;
+	}
+	while(str.charAt(i)==="0"){
+		i++;
+	}
+	while(i<str.length && str.charAt(i)!=="0"){
+		id+=str.charAt(i);
+		i++;
+	}
+	return {
+		firstName: f_Name,
+		lastName: l_name,
+		id: id
+	}
+
 };
 
 // Do not change the code below
