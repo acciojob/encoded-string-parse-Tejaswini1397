45eb784f9d24768e89753bc50976edb14198f07c
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-24 10:08:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const parseCode = (str) => {
   // your code here
+	let res=str.split("000");
+	
+	return res;
 };
 
 // Do not change the code below
 const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
+
